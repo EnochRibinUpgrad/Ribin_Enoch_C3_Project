8194d1342461612773b5d17d19e6bd2f8bac6883
EnochRibinUpgrad <107426786+EnochRibinUpgrad@users.noreply.github.com>
2022-06-15 19:09:06 +0530

    Part 2: Solution

---
+++ Restaurant_project -Student version/src/main/java/itemNotFoundException.java
@@ -0,0 +1,10 @@
+public class itemNotFoundException extends Throwable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 8656535255873723201L;
+
+	public itemNotFoundException(String itemName) {
+        super(itemName);
+    }
+}
---
+++ Restaurant_project -Student version/src/main/java/restaurantNotFoundException.java
@@ -0,0 +1,10 @@
+public class restaurantNotFoundException extends Throwable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 6860141217337981L;
+
+	public restaurantNotFoundException(String restaurantName) {
+        super(restaurantName);
+    }
+}
